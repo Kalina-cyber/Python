8c9f5a58fc154cd797e0c842adc719fb3ea45035
Kalina-cyber <karina.udovenko@nure.ua>
2025-04-02 10:44:15 +0300

    Graph

--- Lb_2/task_1.py
+++ Lb_2/task_1.py
@@ -1,7 +1,10 @@
 import json
 import requests
 
-nbu_response = requests.get("https://bank.gov.ua/NBU_Exchange/exchange_site?start=20250325&end=20250409&valcode=eur&sort=exchangedate&order=asc&json")
+# У порядку зростання
+# nbu_response = requests.get("https://bank.gov.ua/NBU_Exchange/exchange_site?start=20250325&end=20250409&valcode=eur&sort=exchangedate&order=asc&json")
+# У порядку спадання
+nbu_response = requests.get("https://bank.gov.ua/NBU_Exchange/exchange_site?start=20250325&end=20250409&valcode=eur&sort=exchangedate&order=desc&json")
 
 converted_response = json.loads(nbu_response.content)
 
@@ -17,5 +20,8 @@ print("\nDates_massive:", exchange_dates, "\nRates_massive:", exchange_rates)
 
 import matplotlib.pyplot as plt
 
+plt.xlabel("Дата")
+plt.ylabel("Курс (UAH)")
+plt.title("Офіційний курс EUR/UAH")
 plt.plot(exchange_dates, exchange_rates)
 plt.show()
