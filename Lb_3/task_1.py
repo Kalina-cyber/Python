e3e1281ed4d450095e897c2a29d76af6f55175eb
Kalina-cyber <karina.udovenko@nure.ua>
2025-04-23 13:05:38 +0300

    DONE

--- Lb_3/task_1.py
+++ Lb_3/task_1.py
@@ -4,39 +4,38 @@ allowed_ips = ["66.249.73.185", "86.1.76.62"]
 
 def analyze_log_file(log_file_path):
     response_codes = {}
-
     try:
         with open(log_file_path, 'r') as file:
             for line in file:
                 if not line.strip():
                     continue
-
                 parts = line.split('"')
-
                 if len(parts) > 2:
                     status_size = parts[2].strip().split()
-
                     if len(status_size) >= 2:
                         try:
                             status_code = int(status_size[0])
                             response_codes[status_code] = response_codes.get(status_code, 0) + 1
                         except ValueError:
                             continue
-        print(response_codes)
+        print("Аналізатор лог-файлів:\n", response_codes)
         return response_codes
     except FileNotFoundError:
         print("Couldn't find the source file")
         return {}
 
 def generate_file_hashes(file_paths):
+    file_hashes = {}
     for path in file_paths:
         try:
             with open(path, 'rb') as file:
                 content = file.read()
                 file_hash = hashlib.sha256(content).hexdigest()
-                print(f"{path}: {file_hash}")
+                file_hashes[path] = file_hash
         except FileNotFoundError:
             print("Couldn't found the source file")
+    print("Генератор хешів файлів:\n", file_hashes)
+    return file_hashes
 
 def filter_ips(input_file_path, output_file_path, allowed_ips):
     try:
@@ -51,7 +50,7 @@ def filter_ips(input_file_path, output_file_path, allowed_ips):
                         dictionary[ip_Address] += 1
                     else:
                         dictionary[ip_Address] = 1
-        print(dictionary)
+        print("Фільтрація IP-адрес з файлу: \n", dictionary)
         #Import the result in result_logs.txt
         with open(output_file_path, "w") as file:
             for k,v in dictionary.items():
@@ -60,9 +59,6 @@ def filter_ips(input_file_path, output_file_path, allowed_ips):
         print("Couldn't found the source file")
 
 
-print("Аналізатор лог-файлів:")
 analyze_log_file("apache_logs.txt")
-print("Генератор хешів файлів:")
 generate_file_hashes(["apache_logs.txt"])
-print("Фільтрація IP-адрес з файлу:")
 filter_ips("apache_logs.txt", "result_logs.txt", allowed_ips)
